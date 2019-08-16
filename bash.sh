f4d257459e2a2cea5a008a88fa3122d8554e5bb1
漫辰夕 <54102245+ManCxi@users.noreply.github.com>
2019-08-16 10:59:10 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh&&sh install.sh
+yum=`yum repolist | awk -F: '/repolist/{print $2}' |sed 's/,//'`
+[ $yum -gt 0 ] && echo "yum源可用" || echo "yum源不可用"
+rpm -q expect
+if [ $? -eq 0 ];then
+echo "expect 已经安装"
+else
+echo "expect 未安装,正在安装..."
+yum -y install expect
+fi
 
-rm -f /www/server/panel/data/admin_path.pl
+yum install -y wget 
 
-cd /www/server/panel && python tools.py panel 123456
+wget -O install.sh http://download.bt.cn/install/install_6.0.sh
 
-btuser=`cd /www/server/panel && python tools.py panel 123456`
+sh install.sh
+expect <<oppo
+set timeout 10
+expect "Do you want to install Bt-Panel to the /www directory now?(y/n):"       {send "y\r"}
+oppo
+
+if [ $? -eq 0];then
+
+    rm -f /www/server/panel/data/admin_path.pl
+
+    cd /www/server/panel && python tools.py panel 123456
+
+    btuser=`cd /www/server/panel && python tools.py panel 123456`
+
+    btpass=123456
+    
+    echo "宝塔面板地址：http://ip地址:8888
+    用户名:$btuser
+    密码：$btpass"
+    
+    else
+    
+    echo "宝塔安装失败"
+
+fi
