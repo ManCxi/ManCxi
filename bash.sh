bf887789450fab7a0bee0a3380db99722f3b32cf
漫辰夕 <54102245+ManCxi@users.noreply.github.com>
2019-08-16 10:34:37 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh&&sh install.sh
+
+rm -f /www/server/panel/data/admin_path.pl
+
+cd /www/server/panel && python tools.py panel 123456
+
+btuser=`cd /www/server/panel && python tools.py panel 123456`
